02f6f41cf4c750db7c5c9a8d48d4ead27da51744
luobei <48468743+luobei233@users.noreply.github.com>
2023-04-23 21:52:23 +0800

    Add files via upload

---
+++ player/static/js/player.js
@@ -0,0 +1,96 @@
+jks = [
+
+    { url: "https://z1.m1907.cn/?jx=", title: "纯净接口:1907" },
+
+    { url: "https://vip.parwix.com:4433/player/?url=", title: "B站解析:parwix" },
+
+    { url: "https://jsap.attakids.com/?url=", title: "快速接口:百度" },
+
+    { url: "https://www.cuan.la/m3u8.php?url=", title: "B站解析:Mao" },
+
+    { url: "https://www.ckplayer.vip/jiexi/?url=", title: "接口:Ckplayer" },
+
+    { url: "https://api.leduotv.com/wp-api/ifr.php?isDp=1&vid=", title: "接口:乐多资源" },
+
+    { url: "https://ckmov.ccyjjd.com/ckmov/?url=", title: "接口:ccyjjd" },
+
+    { url: "https://jx.m3u8.tv/jiexi/?url=", title: "接口:M3U8" },
+
+    { url: "https://vip.bljiex.com/?v=", title: "接口:BL" },
+
+    { url: "https://www.pangujiexi.cc/jiexi.php?url=", title: "接口:盘古" },
+
+    { url: "https://jx.618g.com/?url=", title: "接口:618G" },
+
+    { url: "https://www.ckmov.vip/api.php?url=", title: "接口:ckmov" },
+
+    { url: "https://123.1dior.cn/?url=", title: "接口:迪奥" },
+
+    { url: "https://jx.rdhk.net/?v=", title: "接口:RDHK" },
+
+    { url: "https://www.h8jx.com/jiexi.php?url=", title: "接口:H8" },
+
+    { url: "https://api.jiexi.la/?url=", title: "接口:解析la" },
+
+    { url: "https://jx.jiubojx.com/vip.php?url=", title: "接口:久播" },
+
+    { url: "https://jx.youyitv.com/?url=", title: "接口:九八" },
+
+    { url: "https://vip.laobandq.com/jiexi.php?url=", title: "接口:老板" },
+
+    { url: "https://jiexi.janan.net/jiexi/?url=", title: "接口:MUTV" },
+    { url: "https://jx.yingxiangbao.cn/vip.php?url=", title: "接口:明日" },
+    { url: "https://jx.wzslw.cn/?url=", title: "接口:磨菇" },
+
+    { url: "https://okjx.cc/?url=", title: "接口:OK" },
+
+    { url: "https://jx.ivito.cn/?url=", title: "接口:维多" },
+
+    { url: "https://www.kpezp.cn/jlexi.php?url=", title: "接口:小蒋" },
+
+    { url: "https://vip.kurumit3.top/?v=", title: "接口:智能" },
+
+    { url: "https://vip.cjys.top/?url=", title: "接口:星驰" },
+
+    { url: "http://60jx.com/?url=", title: "接口:星空" },
+
+    { url: "https://api.yueliangjx.com/?url=", title: "接口:月亮" },
+
+    { url: "https://go.yh0523.cn/y.cy?url=", title: "接口:0523" },
+
+    { url: "https://jx.ergan.top/?url=", title: "接口:云端" },
+
+    { url: "https://jx.yparse.com/index.php?url=", title: "接口:云析" },
+
+    { url: "https://www.1717yun.com/jx/ty.php?url=", title: "接口:17云" },
+
+    { url: "https://api.3jx.top/vip/?url=", title: "接口:66" },
+    { url: "https://vip.66parse.club/?url=", title: "接口:200" },
+    { url: "https://www.8090g.cn/?url=", title: "接口:8090" },
+    { title: "Hk解析", url: "http://jx.rdhk.net/?v=" },
+    { title: "小蒋极致", url: "https://www.kpezp.cn/jlexi.php?url=" },
+    { title: "维多解析（超清）", url: "https://jx.ivito.cn/?url=" },
+    { title: "逆天解析", url: "http://nitian9.com/?url=" },
+    { title: "解析系统", url: "https://www.ckmov.vip/api.php?url=" },
+    { title: "bl解析", url: "https://vip.bljiex.com/?v=" },
+    { title: "凉城解析", url: "http://jx.mw0.cc/?url=" },
+    { title: "宏伟解析", url: "http://www.cqhwdnwx.com/jx/?url=" },
+    { title: "弦易阁", url: "http://jx.hongyishuzhai.com/index.php?url=" },
+    { title: "55解析", url: "http://55jx.top/?url=" },
+    { title: "19解析", url: "http://19g.top/?url=" },
+    { title: "热点解析", url: "http://jx.rdhk.net/?v=" },
+    { title: "ha12解析", url: "http://py.ha12.xyz/sos/index.php?url=" },
+    { url: "http://www.1717yun.com/jx/ty.php?url=", title: "通用vip接口3" },
+
+    { url: "https://www.cuan.la/m3u8.php?url=", title: "③号接口-B站专用接口" },
+    { url: "https://jx.m3u8.tv/jiexi/?url=", title: "⑤号接口" },
+    { url: "http://17kyun.com/api.php?url=", title: "17云" },
+    
+
+
+    { url: "https://60jx.com/?url=", title: "通用高清接口①" },
+    { url: "https://jsap.attakids.com/?url=", title: "通用高清接口②" },
+    { url: "http://www.ikukk.com/?url=", title: "备用高清接口②" },
+    { url: "https://www.cuan.la/m3u8.php?url=", title: "B站番剧接口①" },
+    { url: "https://www.1717yun.com/jx/ty.php?url=", title: "芒果视频接口①" },
+];
